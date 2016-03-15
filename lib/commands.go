f6c4d1d0abbfafb4b5a7f3e6af7249e936ac67f6
Kalle Kankare <kalle.kankare@iki.fi>
2016-03-15 21:12:50 +0200

    add support for prefixed writer and parallel running

--- lib/commands.go
+++ lib/commands.go
@@ -1,33 +1,111 @@
 package gogr
 
 import (
+	"bytes"
+	"errors"
 	"fmt"
+	"io"
 	"os"
 	"os/exec"
+	"path/filepath"
+	"regexp"
+	"strconv"
+	"sync"
 )
 
+type PrefixedWriter struct {
+	Prefix string
+	writer io.Writer
+}
+
+func NewPrefixedWriter(prefix string, writer io.Writer) (ret PrefixedWriter) {
+	ret.Prefix = prefix
+	ret.writer = writer
+	return
+}
+
+func (p *PrefixedWriter) Write(buf []byte) (n int, err error) {
+	var wr = func(buf []byte) bool {
+		_, err = p.writer.Write(buf)
+		if err != nil {
+			return false
+		}
+		return true
+	}
+
+	n = len(buf)
+
+	last := false
+	pos := bytes.IndexRune(buf, '\n')
+	for !last {
+		if pos == -1 {
+			pos = len(buf) - 1
+			last = true
+		}
+
+		if len(buf) > 0 && (!wr([]byte(p.Prefix)) || !wr(buf[:pos+1])) {
+			return
+		}
+
+		if !last {
+			buf = buf[pos+1:]
+			pos = bytes.IndexRune(buf, '\n')
+		}
+	}
+	return
+}
+
 func RunCommand(directory string, program string, args ...string) (err error) {
-	// fmt.Println(directory)
-	// fmt.Println(program)
+	dir := filepath.Base(directory)
+	pwo := NewPrefixedWriter(fmt.Sprintf("%s: ", dir), os.Stderr)
+	pwe := NewPrefixedWriter(fmt.Sprintf("%s(err): ", dir), os.Stderr)
 	cmd := exec.Command(program, args...)
 	cmd.Dir = directory
-	cmd.Stdout = os.Stdout
-	cmd.Stderr = os.Stderr
+	cmd.Stdout = &pwo
+	cmd.Stderr = &pwe
 
 	err = cmd.Run()
 	if err != nil {
+		fmt.Fprintf(&pwe, "Command failed: %s\n", err)
 		return
 	}
 
 	return
 }
 
+func RunCommands(opts Options, dirs []string, args []string) (err error) {
+	concurrent, err := strconv.ParseBool(opts.Get("concurrent", "false"))
+	if err != nil {
+		return
+	}
+
+	if concurrent {
+		wg := sync.WaitGroup{}
+		for _, dir := range dirs {
+			wg.Add(1)
+			go func(dir string) {
+				defer wg.Done()
+				_ = RunCommand(dir, args[0], args[1:]...)
+			}(dir)
+		}
+		wg.Wait()
+	} else {
+		for _, dir := range dirs {
+			_ = RunCommand(dir, args[0], args[1:]...)
+		}
+	}
+	return
+}
+
+func isDirectory(path string) bool {
+	info, err := os.Stat(path)
+	return err == nil && info.IsDir()
+}
+
 func ParseDirectories(args []string) (dirs []string, rest []string, err error) {
-	var info os.FileInfo
 	pos := -1
 	for i, arg := range args {
-		info, err = os.Stat(arg)
-		if err != nil || !info.IsDir() {
+		if !isDirectory(arg) {
 			pos = i
 			err = nil
 			break
