081f949a2dfbb49e69239e194a0524c51e4688cf
Kalle Kankare <kalle.kankare@iki.fi>
2016-03-19 21:19:41 +0200

    discover: cleanup

--- lib/discover.go
+++ lib/discover.go
@@ -9,12 +9,6 @@ import (
 	"strings"
 )
 
-type endOfProcessing bool
-
-func (endOfProcessing) Error() string {
-	return "End of processing"
-}
-
 func Discover(opts Options, root string, file string) (dirs []string, err error) {
 	maxDepth, err := strconv.ParseInt(opts.Get("discover-max-depth", "5"), 10, 0)
 	if err != nil {
@@ -25,14 +19,12 @@ func Discover(opts Options, root string, file string) (dirs []string, err error)
 	dw := func(path string, info os.FileInfo, err error) (ret error) {
 		relpath, err := filepath.Rel(root, path)
 		pathlist := strings.Split(relpath, string(filepath.Separator))
-		// fmt.Println(pathlist, len(pathlist))
 
 		if len(pathlist) >= int(maxDepth) {
 			ret = filepath.SkipDir
 			return
 		}
 		if pathlist[len(pathlist)-1] == file {
-			// fmt.Println("Found",path,"as directory", filepath.Dir(path))
 			dirs = append(dirs, filepath.Dir(path))
 		}
 
@@ -40,7 +32,5 @@ func Discover(opts Options, root string, file string) (dirs []string, err error)
 	}
 
 	err = filepath.Walk(root, dw)
-
-	// fmt.Println("Hakemistot", dirs)
 	return
 }
