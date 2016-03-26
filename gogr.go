617b2f38a57e53ca1a4cbc8542f07982caa1f2e0
Kalle Kankare <kalle.kankare@iki.fi>
2016-03-26 10:32:15 +0200

    gogr: piece down main()

--- gogr.go
+++ gogr.go
@@ -19,6 +19,9 @@ var (
 	progVersion  = majorVersion + "-" + version
 )
 
+var tagman gogr.TagManager
+var opts gogr.Options
+
 func printErr(err error, message string, arg ...string) {
 	msg := ""
 	if err != nil {
@@ -67,9 +70,102 @@ func rmTag(tagman gogr.TagManager, tag string, dirs []string) {
 	}
 }
 
+func cmdTag(cmd *cli.Cmd) {
+	parseDir := func(dirs []string) (ret []string) {
+		if len(dirs) > 0 {
+			_, rest, err := gogr.ParseDirectories(dirs)
+			if err != nil {
+				fault(err, "Directory parsing failed")
+			}
+			if len(rest) != 0 {
+				fault(nil, "The following are not directories: ", rest...)
+			}
+			ret = dirs
+		}
+		return
+	}
+
+	cmd.Command("add", "Add tag to path", func(cmd *cli.Cmd) {
+		cmd.Spec = "TAG [DIR...]"
+		tagArg := cmd.StringArg("TAG", "", "Tag to add")
+		dirArg := cmd.StringsArg("DIR", nil, "Directories to add the tag to")
+
+		cmd.Action = func() {
+			dirs := parseDir(*dirArg)
+			addTag(tagman, *tagArg, dirs)
+		}
+	})
+	cmd.Command("rm", "Remove tag from paths", func(cmd *cli.Cmd) {
+		cmd.Spec = "TAG [DIR...]"
+		tagArg := cmd.StringArg("TAG", "", "Tag to remove")
+		dirArg := cmd.StringsArg("DIR", nil, "Directories to remove the tag from")
+
+		cmd.Action = func() {
+			rmTag(tagman, *tagArg, parseDir(*dirArg))
+		}
+	})
+
+	cmd.Command("list", "List all tags or directories of given tag. This is the default action.", func(cmd *cli.Cmd) {
+		cmd.Spec = "[TAG]"
+
+		tagArg := cmd.StringArg("TAG", "", "List directories of this tag")
+
+		cmd.Action = func() {
+			if *tagArg == "" {
+				for tag := range tagman.Tags {
+					fmt.Println(tag)
+				}
+			} else {
+				dirs := tagman.Dirs([]string{*tagArg}, nil)
+				for _, dir := range dirs {
+					fmt.Println(dir)
+				}
+			}
+		}
+	})
+
+	cmd.Action = func() {
+		for tag := range tagman.Tags {
+			fmt.Println(tag)
+		}
+	}
+}
+
+func cmdDiscover(cmd *cli.Cmd) {
+	optDepth := cmd.IntOpt("max-depth d", 5, "Maximum depth of discovery")
+	wd, err := os.Getwd()
+	if err != nil {
+		fault(err, "Could not get the current directory")
+	}
+	tagArg := cmd.StringArg("TAG", "", "Tag to add or update")
+	optRoots := cmd.StringsArg("ROOT", []string{wd},
+		"Root directory for the discovery")
+	optFile := cmd.StringOpt("file f", ".git",
+		"File or directory to discover")
+	cmd.Spec = "[OPTIONS] TAG [ROOT...]"
+
+	cmd.Action = func() {
+		var dirs []string
+		opts.Set("discover-max-depth", strconv.Itoa(*optDepth))
+		for _, root := range *optRoots {
+			tmp, err := gogr.Discover(opts, root, *optFile)
+			if err != nil {
+				fault(err, "Discovery failed")
+			}
+
+			dirs = append(dirs, tmp...)
+		}
+		for _, dir := range dirs {
+			fmt.Println(dir)
+		}
+
+		rmTag(tagman, *tagArg, []string{})
+		addTag(tagman, *tagArg, dirs)
+	}
+}
+
 func main() {
-	var tagman gogr.TagManager
-	opts := gogr.GetOptions()
+	opts = gogr.GetOptions()
 
 	opts.Set("application-name", progName)
 	opts.Set("configuration-file", "config.json")
@@ -93,100 +189,8 @@ func main() {
 		opts.Set("concurrent", strconv.FormatBool(*optConcurrent))
 	}
 
-	app.Command("tag", "Tag management", func(cmd *cli.Cmd) {
-
-		parseDir := func(dirs []string) (ret []string) {
-			if len(dirs) > 0 {
-				_, rest, err := gogr.ParseDirectories(dirs)
-				if err != nil {
-					fault(err, "Directory parsing failed")
-				}
-				if len(rest) != 0 {
-					fault(nil, "The following are not directories: ", rest...)
-				}
-				ret = dirs
-			}
-			return
-		}
-
-		cmd.Command("add", "Add tag to path", func(cmd *cli.Cmd) {
-			cmd.Spec = "TAG [DIR...]"
-			tagArg := cmd.StringArg("TAG", "", "Tag to add")
-			dirArg := cmd.StringsArg("DIR", nil, "Directories to add the tag to")
-
-			cmd.Action = func() {
-				dirs := parseDir(*dirArg)
-				addTag(tagman, *tagArg, dirs)
-			}
-		})
-		cmd.Command("rm", "Remove tag from paths", func(cmd *cli.Cmd) {
-			cmd.Spec = "TAG [DIR...]"
-			tagArg := cmd.StringArg("TAG", "", "Tag to remove")
-			dirArg := cmd.StringsArg("DIR", nil, "Directories to remove the tag from")
-
-			cmd.Action = func() {
-				rmTag(tagman, *tagArg, parseDir(*dirArg))
-			}
-		})
-
-		cmd.Command("list", "List all tags or directories of given tag. This is the default action.", func(cmd *cli.Cmd) {
-			cmd.Spec = "[TAG]"
-
-			tagArg := cmd.StringArg("TAG", "", "List directories of this tag")
-
-			cmd.Action = func() {
-				if *tagArg == "" {
-					for tag := range tagman.Tags {
-						fmt.Println(tag)
-					}
-				} else {
-					dirs := tagman.Dirs([]string{*tagArg}, nil)
-					for _, dir := range dirs {
-						fmt.Println(dir)
-					}
-				}
-			}
-		})
-
-		cmd.Action = func() {
-			for tag := range tagman.Tags {
-				fmt.Println(tag)
-			}
-		}
-	})
-
-	app.Command("discover", "Discover directories containing a certain file", func(cmd *cli.Cmd) {
-		optDepth := cmd.IntOpt("max-depth d", 5, "Maximum depth of discovery")
-		wd, err := os.Getwd()
-		if err != nil {
-			fault(err, "Could not get the current directory")
-		}
-		tagArg := cmd.StringArg("TAG", "", "Tag to add or update")
-		optRoots := cmd.StringsArg("ROOT", []string{wd},
-			"Root directory for the discovery")
-		optFile := cmd.StringOpt("file f", ".git",
-			"File or directory to discover")
-		cmd.Spec = "[OPTIONS] TAG [ROOT...]"
-
-		cmd.Action = func() {
-			var dirs []string
-			opts.Set("discover-max-depth", strconv.Itoa(*optDepth))
-			for _, root := range *optRoots {
-				tmp, err := gogr.Discover(opts, root, *optFile)
-				if err != nil {
-					fault(err, "Discovery failed")
-				}
-
-				dirs = append(dirs, tmp...)
-			}
-			for _, dir := range dirs {
-				fmt.Println(dir)
-			}
-
-			rmTag(tagman, *tagArg, []string{})
-			addTag(tagman, *tagArg, dirs)
-		}
-	})
+	app.Command("tag", "Tag management", cmdTag)
+	app.Command("discover", "Discover directories containing a certain file", cmdDiscover)
 
 	argArg := app.StringsArg("ARG", nil, "Directories and command to be run")
 
