2b9e40a3dd9eb5e6e2eb3a137ef724e133709cd1
Kalle Kankare <kalle.kankare@iki.fi>
2016-06-02 11:20:13 +0300

    Simplify PrefixedWriter again
    Write everything with a single call to the underlying io.Writer. This
    prevents concurrent writing to cut off during writing a line.

--- lib/prefixed-writer.go
+++ lib/prefixed-writer.go
@@ -17,24 +17,22 @@ func NewPrefixedWriter(prefix string, writer io.Writer) (ret PrefixedWriter) {
 }
 
 func (p *PrefixedWriter) Write(buf []byte) (n int, err error) {
-	var wr = func(buf []byte) bool {
-		_, err = p.writer.Write(buf)
-		if err != nil {
-			return false
-		}
-		return true
-	}
-
 	n = len(buf)
 
 	pos := -1
+	line := make([]byte, 0, 1024)
 	for len(buf) > 0 {
 		pos = bytes.IndexRune(buf, '\n')
 		if pos == -1 {
 			pos = len(buf) - 1
 		}
 
-		if !wr([]byte(p.Prefix)) || !wr(buf[:pos+1]) {
+		line = line[:0]
+		line = append(line, []byte(p.Prefix)...)
+		line = append(line, buf[:pos+1]...)
+
+		_, err = p.writer.Write(line)
+		if err != nil {
 			return
 		}
 
