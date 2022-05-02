97d729dffee45299ad3b8c84edda233fb3c1ae9a
Kalle Kankare <kalle.kankare@iki.fi>
2022-05-03 01:11:28 +0300

    config: Change the xdg dependency to a versioned one

--- lib/config.go
+++ lib/config.go
@@ -1,13 +1,15 @@
 package gogr
 
 import (
-	"github.com/casimir/xdg-go"
+	"path/filepath"
+
+	"github.com/OpenPeeDeeP/xdg"
 	"github.com/kopoli/appkit"
 )
 
 // DefaultConfigFile gets the default configuration file name based on given
 // appkit.Options
 func DefaultConfigFile(opts appkit.Options) string {
-	app := xdg.App{Name: opts.Get("application-name", "gogr")}
-	return app.ConfigPath(opts.Get("configuration-file", "config.json"))
+	path := xdg.New("", opts.Get("application-name", "gogr")).ConfigHome()
+	return filepath.Join(path, opts.Get("configuration-file", "config.json"))
 }
