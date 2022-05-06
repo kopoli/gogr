9fd2fa6d49bee6cf1bfd1ef15e6b66e78885317c
Kalle Kankare <kalle.kankare@iki.fi>
2022-05-06 22:01:15 +0300

    gogr: Replace CLI with appkit

--- go.mod
+++ go.mod
@@ -4,6 +4,11 @@ go 1.17
 
 require (
 	github.com/OpenPeeDeeP/xdg v1.0.0
-	github.com/jawher/mow.cli v1.2.0
-	github.com/kopoli/appkit v0.9.1
+	github.com/kopoli/appkit v0.12.0
+)
+
+require (
+	github.com/stretchr/objx v0.2.0 // indirect
+	github.com/stretchr/testify v1.4.0 // indirect
+	gopkg.in/yaml.v2 v2.2.5 // indirect
 )
--- gogr.go
+++ gogr.go
@@ -1,53 +1,38 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"os"
-	"runtime"
 	"strconv"
 	"strings"
 
-	cli "github.com/jawher/mow.cli"
 	"github.com/kopoli/appkit"
 	gogr "github.com/kopoli/gogr/lib"
 )
 
 var (
-	progName     = "gogr"
-	majorVersion = "1"
-	version      = "development"
-	timestamp    = "undefined"
-	progVersion  = majorVersion + "-" + version
+	version     = "Undefined"
+	timestamp   = "Undefined"
+	buildGOOS   = "Undefined"
+	buildGOARCH = "Undefined"
+	progVersion = "" + version
 )
 
 var tagman gogr.TagManager
 var opts appkit.Options
 
-func printErr(err error, message string, arg ...string) {
-	msg := ""
-	if err != nil {
-		msg = fmt.Sprintf(" (error: %s)", err)
-	}
-	fmt.Fprintf(os.Stderr, "Error: %s%s.%s\n", message, strings.Join(arg, " "), msg)
-}
-
 func fault(err error, message string, arg ...string) {
-	printErr(err, message, arg...)
-	cli.Exit(1)
-}
-
-func faultShowHelp(app *cli.Cli, message string, arg ...string) {
-	printErr(nil, message, arg...)
-	app.PrintLongHelp()
-	cli.Exit(1)
+	if err != nil {
+		_, _ = fmt.Fprintf(os.Stderr, "Error: %s%s: %s\n", message, strings.Join(arg, " "), err)
+		os.Exit(1)
+	}
 }
 
 func addTag(tagman gogr.TagManager, tag string, dirs []string) {
 	if len(dirs) == 0 {
 		wd, err := os.Getwd()
-		if err != nil {
-			fault(err, "Getting working directory failed")
-		}
+		fault(err, "Getting working directory failed")
 		dirs = append(dirs, wd)
 	}
 	if !tagman.ValidateTag(tag) {
@@ -62,163 +47,161 @@ func addTag(tagman gogr.TagManager, tag string, dirs []string) {
 
 func rmTag(tagman gogr.TagManager, tag string, dirs []string) {
 	if !tagman.ValidateTag(tag) {
-		fault(nil, "Improper tag string")
+		fault(fmt.Errorf("parsing failed"), "Improper tag string")
 	}
 	tagman.Remove(tag, dirs...)
 	err := tagman.Save()
-	if err != nil {
-		fault(err, "Saving configuration failed.")
-	}
+	fault(err, "Saving configuration failed.")
 }
 
-func cmdTag(cmd *cli.Cmd) {
-	parseDir := func(dirs []string) (ret []string) {
+func main() {
+	opts = appkit.NewOptions()
+
+	opts.Set("program-name", os.Args[0])
+	opts.Set("program-version", progVersion)
+	opts.Set("program-timestamp", timestamp)
+	opts.Set("program-buildgoos", buildGOOS)
+	opts.Set("program-buildgoarch", buildGOARCH)
+	opts.Set("configuration-file", "config.json")
+
+	base := appkit.NewCommand(nil, "", "Run commands in multiple directories")
+	base.ArgumentHelp = "| [+-]@<tag> [CMD ...]"
+	optVersion := base.Flags.Bool("version", false, "Display version")
+	base.Flags.BoolVar(optVersion, "v", false, "Display version")
+	optVerbose := base.Flags.Bool("verbose", false, "Print verbose output")
+	base.Flags.BoolVar(optVerbose, "V", false, "Print verbose output")
+
+	optConfig := base.Flags.String("config", gogr.DefaultConfigFile(opts), "Configuration file")
+	base.Flags.StringVar(optConfig, "c", gogr.DefaultConfigFile(opts), "Configuration file")
+	optConcurrent := base.Flags.Bool("concurrent", false, "Run the commands concurrently")
+	base.Flags.BoolVar(optConcurrent, "j", false, "Run the commands concurrently")
+	optLicenses := base.Flags.Bool("licenses", false, "Display the licenses")
+
+	tag := appkit.NewCommand(base, "tag", "Tag management")
+	tag.SubCommandHelp = "<COMMAND>"
+	tlist := appkit.NewCommand(tag, "list", "List all tags or directories of given tag. This is the default action.")
+	tlist.ArgumentHelp = "[TAG ...]"
+	tadd := appkit.NewCommand(tag, "add", "Add tag to path")
+	tadd.ArgumentHelp = "TAG [DIR ...]"
+	trm := appkit.NewCommand(tag, "rm", "Remove tag from paths")
+	trm.ArgumentHelp = "TAG [DIR ...]"
+
+	discover := appkit.NewCommand(base, "discover", "Discover directories containing a certain file")
+	discover.ArgumentHelp = "TAG [ROOT ...]"
+	optDepth := discover.Flags.Int("max-depth", 5, "Maximum depth of discovery")
+	optFile := discover.Flags.String("File", ".git", "File or directory do discover")
+
+	err := base.Parse(os.Args[1:], opts)
+	if err == flag.ErrHelp {
+		os.Exit(0)
+	}
+
+	errorShowHelp := func(message string) {
+		fmt.Fprintf(os.Stderr, "Error: %s\n\n", message)
+		base.Flags.Usage()
+		os.Exit(1)
+	}
+
+	if *optVersion {
+		fmt.Println(appkit.VersionString(opts))
+		os.Exit(0)
+	}
+	opts.Set("configuration-file", *optConfig)
+	if *optVerbose {
+		opts.Set("flag-verbose", "t")
+	}
+	if *optConcurrent {
+		opts.Set("concurrent", "t")
+	}
+	if *optLicenses {
+		l, err := GetLicenses()
+		fault(err, "Getting licenses failed")
+		s, err := appkit.LicenseString(l)
+		fault(err, "Interpreting licenses failed")
+		fmt.Print(s)
+		os.Exit(0)
+	}
+
+	opts.Set("discover-max-depth", strconv.Itoa(*optDepth))
+	opts.Set("discover-file", *optFile)
+
+	cmd := opts.Get("cmdline-command", "")
+	argstr := opts.Get("cmdline-args", "")
+	args := appkit.SplitArguments(argstr)
+
+	if cmd == "" && argstr == "" {
+		errorShowHelp("Arguments required")
+	}
+
+	tagman = gogr.NewTagManager(opts)
+
+	parseDir := func(dirs []string) ([]string) {
+		ret := []string{}
 		if len(dirs) > 0 {
 			_, rest, err := gogr.ParseDirectories(dirs)
-			if err != nil {
-				fault(err, "Directory parsing failed")
-			}
+			fault(err, "Directory parsing failed")
 			if len(rest) != 0 {
-				fault(nil, "The following are not directories: ", rest...)
+				fault(fmt.Errorf("not directories: %v", rest), "Invalid arguments")
 			}
 			ret = dirs
 		}
-		return
+		return ret
 	}
 
-	cmd.Command("add", "Add tag to path", func(cmd *cli.Cmd) {
-		cmd.Spec = "TAG [DIR...]"
-		tagArg := cmd.StringArg("TAG", "", "Tag to add")
-		dirArg := cmd.StringsArg("DIR", nil, "Directories to add the tag to")
-
-		cmd.Action = func() {
-			dirs := parseDir(*dirArg)
-			addTag(tagman, *tagArg, dirs)
+	parseTagDirArg := func(args []string) (tag string, dirs []string) {
+		if len(args) < 1 {
+			fault(fmt.Errorf("Not enough arguments"), "Command line parsing failed")
 		}
-	})
-	cmd.Command("rm", "Remove tag from paths", func(cmd *cli.Cmd) {
-		cmd.Spec = "TAG [DIR...]"
-		tagArg := cmd.StringArg("TAG", "", "Tag to remove")
-		dirArg := cmd.StringsArg("DIR", nil, "Directories to remove the tag from")
 
-		cmd.Action = func() {
-			rmTag(tagman, *tagArg, parseDir(*dirArg))
-		}
-	})
-
-	listTags := func() {
-		for tag := range tagman.Tags {
-			fmt.Println(tag)
-		}
+		return args[0], args[1:]
 	}
 
-	cmd.Command("list", "List all tags or directories of given tag. This is the default action.", func(cmd *cli.Cmd) {
-		cmd.Spec = "[TAG]"
-
-		tagArg := cmd.StringArg("TAG", "", "List directories of this tag")
-
-		cmd.Action = func() {
-			if *tagArg == "" {
-				listTags()
-			} else {
-				dirs := tagman.Dirs([]string{*tagArg}, nil)
-				for _, dir := range dirs {
-					fmt.Println(dir)
-				}
+	switch cmd {
+	case "tag":
+		fallthrough
+	case "tag list":
+		if len(args) == 0 {
+			for tag := range tagman.Tags {
+				fmt.Println(tag)
+			}
+		} else {
+			dirs := tagman.Dirs(args, nil)
+			for _, dir := range dirs {
+				fmt.Println(dir)
 			}
 		}
-	})
-
-	cmd.Action = listTags
-}
-
-func cmdDiscover(cmd *cli.Cmd) {
-	optDepth := cmd.IntOpt("max-depth d", 5, "Maximum depth of discovery")
-	wd, err := os.Getwd()
-	if err != nil {
-		fault(err, "Could not get the current directory")
-	}
-	tagArg := cmd.StringArg("TAG", "", "Tag to add or update")
-	optRoots := cmd.StringsArg("ROOT", []string{wd},
-		"Root directory for the discovery")
-	optFile := cmd.StringOpt("file f", ".git",
-		"File or directory to discover")
-	cmd.Spec = "[OPTIONS] TAG [ROOT...]"
-
-	cmd.Action = func() {
+	case "tag add":
+		tag, dirs := parseTagDirArg(args)
+		addTag(tagman, tag, parseDir(dirs))
+	case "tag rm":
+		tag, dirs := parseTagDirArg(args)
+		rmTag(tagman, tag, parseDir(dirs))
+	case "discover":
+		tag, roots := parseTagDirArg(args)
+		if len(roots) == 0 {
+			wd, err := os.Getwd()
+			fault(err, "Could not get the current directory")
+			roots = append(roots, wd)
+		}
+	
 		var dirs []string
-		opts.Set("discover-max-depth", strconv.Itoa(*optDepth))
-		for _, root := range *optRoots {
+		for _, root := range roots {
 			tmp, err := gogr.Discover(opts, root, *optFile)
-			if err != nil {
-				fault(err, "Discovery failed")
-			}
-
+			fault(err, "Discovery failed")
 			dirs = append(dirs, tmp...)
 		}
 		for _, dir := range dirs {
 			fmt.Println(dir)
 		}
 
-		rmTag(tagman, *tagArg, []string{})
-		addTag(tagman, *tagArg, dirs)
-	}
-}
-
-func main() {
-	opts = appkit.NewOptions()
-
-	opts.Set("application-name", progName)
-	opts.Set("configuration-file", "config.json")
-
-	app := cli.App(progName, "Run commands in multiple directories")
-	app.Spec = "[OPTIONS] [-- ARG...]"
-
-	app.Version("version v", fmt.Sprintf(
-		"%s: %s\nBuilt with: %s/%s on %s/%s\nBuilt: %s",
-		progName, progVersion, runtime.Compiler, runtime.Version(),
-		runtime.GOOS, runtime.GOARCH, timestamp))
-
-	optVerbose := app.BoolOpt("verbose V", false, "Print verbose output")
-	optConfig := app.StringOpt("config c", gogr.DefaultConfigFile(opts), "Configuration file")
-	optConcurrent := app.BoolOpt("concurrent j", false, "Run the commands concurrently")
-	optLicenses := app.BoolOpt("licenses l", false, "Display the licenses")
-
-	app.Before = func() {
-		opts.Set("configuration-file", *optConfig)
-		tagman = gogr.NewTagManager(opts)
-		opts.Set("flag-verbose", strconv.FormatBool(*optVerbose))
-		opts.Set("concurrent", strconv.FormatBool(*optConcurrent))
-	}
-
-	app.Command("tag", "Tag management", cmdTag)
-	app.Command("discover", "Discover directories containing a certain file", cmdDiscover)
-
-	argArg := app.StringsArg("ARG", nil, "Directories and command to be run")
-
-	app.Action = func() {
-		if *optLicenses {
-			l, err := GetLicenses()
-			if err != nil {
-				fault(err, "Getting licenses failed")
-			}
-			s, err := appkit.LicenseString(l)
-			if err != nil {
-				fault(err, "Interpreting licenses failed")
-			}
-			fmt.Print(s)
-			return
-		}
-		if len(*argArg) == 0 {
-			faultShowHelp(app, "Arguments required")
-		}
-		tagitems := gogr.ParseTags(*argArg)
+		rmTag(tagman, tag, []string{})
+		addTag(tagman, tag, dirs)
+	default:
+		tagitems := gogr.ParseTags(args)
 		vt, err := gogr.VerifyTags(tagitems)
-		if err != nil {
-			fault(err, "Parsing arguments failed")
-		}
+		fault(err, "Parsing arguments failed")
 		if len(vt.Dirs) == 0 && len(vt.Tags) == 0 {
-			faultShowHelp(app, "Directories or tags are required")
+			errorShowHelp("Directories or tags are required")
 		}
 
 		if vt.Command.Str != "" {
@@ -230,26 +213,20 @@ func main() {
 			case gogr.None:
 				fallthrough
 			default:
-				fault(nil, "Internal error: improper command")
+				fault(fmt.Errorf("improper command"), "Internal error")
 			}
 			return
 		}
 
 		invalid := tagman.AreProper(vt.Tags)
 		if len(invalid) > 0 {
-			fault(nil, "Improper tags: ", strings.Join(invalid, ", "))
+			fault(fmt.Errorf("improper tags"),
+				"Tags: ", strings.Join(invalid, ", "))
 		}
 
 		vt.Dirs = tagman.Dirs(vt.Tags, vt.Dirs)
 
 		err = gogr.RunCommands(opts, vt.Dirs, vt.Args)
-		if err != nil {
-			fault(err, "Running command failed")
-		}
-	}
-
-	err := app.Run(os.Args)
-	if err != nil {
-		fault(err, "Argument parsing failed")
+		fault(err, "Running command failed")
 	}
 }
