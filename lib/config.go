27bc1bc070ddbcad35cc4f200cb4dc501234ef98
Kalle Kankare <kalle.kankare@iki.fi>
2022-06-18 13:14:58 +0300

    config: Add option to override the DefaultConfigFile

--- lib/config.go
+++ lib/config.go
@@ -9,7 +9,9 @@ import (
 
 // DefaultConfigFile gets the default configuration file name based on given
 // appkit.Options
-func DefaultConfigFile(opts appkit.Options) string {
+func defaultConfigFile(opts appkit.Options) string {
 	path := xdg.New("", opts.Get("application-name", "gogr")).ConfigHome()
 	return filepath.Join(path, opts.Get("configuration-file", "config.json"))
 }
+
+var DefaultConfigFile = defaultConfigFile
